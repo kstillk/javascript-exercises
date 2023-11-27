ef0ddf7c642b6343833094ec9e92d32e77e1be24
kstilk <150075818+kstillk@users.noreply.github.com>
2023-11-27 22:32:59 +0300

    Solve issues

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    const arr = Array.from(arguments);
+    const arrSub = Array.from(arguments[0])
+    for (i = 1; i <= arr.length; i++) {
+        for (isub = 0; isub <= arrSub.length; isub++) {
+            if (arr[i] === arrSub[isub]) {
+                arrSub.splice(isub, 1);
+            }
+        }
+    }
+    return arrSub;
 };
 
 // Do not edit below this line
