d6b2343d3048feb4aa8f5ad77663fb774b038aba
kstilk <150075818+kstillk@users.noreply.github.com>
2023-11-29 16:26:38 +0300

    Solve issues

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fh) {
+  let resultCs = (5 / 9) * (fh - 32);
+  return Math.round(resultCs * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cs) {
+  let resultFh = 9 / 5 * cs + 32;
+  return Math.round(resultFh * 10) / 10;
 };
 
 // Do not edit below this line
